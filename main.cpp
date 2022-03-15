71b4a54234c0f06e106628b8bc0f58951c767a27
FredericALP <101244166+Fred-23@users.noreply.github.com>
2022-03-15 08:28:20 +0000

    Creation et tests des contructeurs à l'origine, qui prend deux paramètres réels et de la fonction translater

--- main.cpp
+++ main.cpp
@@ -2,17 +2,52 @@
 using namespace std;
 
 class thispoint {
+private :
+
+  int x;
+  int y;
+  
+  int a;
+  int b;
 public:
-  void translater(int x, int y){
-//    cout"
+  void translater(thispoint Trans){
+    this->x += Trans.x;
+    this->y += Trans.y;
+  }
+
+  thispoint(){
+    this->x = 0;
+    this->y = 0;
+  }
+
+  thispoint(int a, int b){
+    this->x = a;
+    this->y = b;
+  }
+  
+
+  thispoint(thispoint const& ref_point){
+    this->x = ref_point.x;
+    this->y = ref_point.y;
+  }
+  void print(){
+    cout<<"x"<<x<<endl;
+    cout<<"y"<<y<<endl;
   }
 };
-
-//Mettre un constructeur
-
 
 
 
 int main() { 
-    cout << "solde du compte janick : " <<  endl;
+    //cout << "solde du compte janick : " <<  endl;
+  thispoint p(2,5);
+  thispoint p2(3,3);
+
+  p.print();
+
+  p.translater(p2);
+  
+  //p.thispoint(c,d);
+  p.print();
+  
 }
