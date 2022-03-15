83c86b79f20bff6ad63bf0bd6674ad072071ab61
wroman <101247742+roman-wolfensperger@users.noreply.github.com>
2022-03-15 08:48:13 +0000

    ajout des accesseurs x et y

--- main.cpp
+++ main.cpp
@@ -10,36 +10,36 @@ private :
   int a;
   int b;
 public:
-  void translater(thispoint Trans){
+  void translater(thispoint Trans){ //fonction translation
     this->x += Trans.x;
     this->y += Trans.y;
   }
 
-  thispoint(){
+  thispoint(void){ //constructeur initialisation origine
     this->x = 0;
     this->y = 0;
   }
 
-  thispoint(int a, int b){
+  thispoint(int a, int b){ //constructeur à partir d'entier
     this->x = a;
     this->y = b;
   }
   
-
-  thispoint(thispoint const& ref_point){
+  thispoint(thispoint const& ref_point){ //constructeur reference
     this->x = ref_point.x;
     this->y = ref_point.y;
   }
-  void print(){
+  void print(){ //affichage des points
     cout<<"x"<<x<<endl;
     cout<<"y"<<y<<endl;
   }
+
+  //accesseurs
+  void setx(int x)  {  this->x = x; }
+  void sety(int y)  {  this->y = y; }
 };
 
-
-
-int main() { 
-  
+int main() {
   thispoint p(2,5);
   thispoint p2(3,3);
   thispoint p3(p2); //construction référence
@@ -47,6 +47,10 @@ int main() {
 
   p.translater(p2); //translation de p par p2
   p.print(); //après translation
-  p2.print();
+
   p3.print();
+
+  p2.setx(9); //test d'un accesseur
+  p2.print();
 }
+
