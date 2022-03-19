3fa55c8e6fcfeaca266e176dd8919470b782b7a6
wroman <101247742+roman-wolfensperger@users.noreply.github.com>
2022-03-19 22:15:44 +0000

    début création classe cercle

--- main.cpp
+++ main.cpp
@@ -3,13 +3,11 @@ using namespace std;
 
 class thispoint {
 private :
-
-  int x;
-  int y;
-  
   int a;
   int b;
 public:
+   int x;
+   int y;
   void translater(thispoint Trans){ //fonction translation
     this->x += Trans.x;
     this->y += Trans.y;
@@ -57,9 +55,66 @@ thispoint & thispoint::operator+=(const thispoint & pointtrans){
   return *this;
 };
 
+class Forme {
+	private :
+	public :
+  thispoint Point_centre;
+
+  //méthodes abstraites
+  void perimetre(Forme Point_centre);
+  void surface(Forme Point_centre);
+
+    Forme& operator+=(const thispoint & pointtrans);
+    friend ostream& operator<<(ostream &os,const Forme &Fot);
+    
+    Forme(thispoint const& ref_point){ //constructeur reference
+    	this->Point_centre.x = ref_point.x;
+   		this->Point_centre.y = ref_point.y;
+ 		}
+};
+
+Forme& Forme::operator+=(const thispoint& pointtrans){
+  Point_centre.x += pointtrans.x;
+  Point_centre.y += pointtrans.y;
+  return *this;
+};
+
+ostream& operator<< (ostream &os,const Forme &Fot){ //utilisation cout<<Point 
+  os<<"Forme X value "<<Fot.Point_centre.x<<endl;
+  os<<"Forme Y value "<<Fot.Point_centre.y<<endl;
+  return os;
+};
+
+//***********************FORME CERCLE******************************//
+//Classe hérité de forme auquel on ajoute des paramètres// Voir les classes hérités
+class Cercle {
+	private :
+    Forme frm_Cercle;
+    int rayon;
+	public :
+
+    Cercle(Forme const&ref_forme, int r){ 
+    	/*this->frm_Cercle.Point_centre.x = ref_forme.Point_centre.x;
+      this->frm_Cercle.Point_centre.y = ref_forme.Point_centre.y;*/
+      this->frm_Cercle = ref_forme;
+      this->rayon=r;
+ 		}
+
+};
+//***********************FORME RECTANGLE******************************//
+
+//***********************FORME CARRE******************************//
+
+
+
 int main() {
   thispoint p2(3,3);
   thispoint p(1,1);
   p+=p2;
-  cout<<p;
+  Forme f(p);
+  cout<<f;
+  f+=p2;
+  cout<<f;
+  //int ray = 5;
+  //Cercle(f,5);
 }
